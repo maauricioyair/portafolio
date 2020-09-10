814f152d3cd5f84be5651a265dfd3dc925c5dd0c
Mauricio Yair <yairchavez555@gmail.com>
2020-09-09 19:40:33 -0500

    Version 0.01

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particlesjs.json');
+
+
