a2604f227ab67bd34cc30f377cd84e3d5a64534d
Mauricio Yair <yairchavez555@gmail.com>
2022-10-26 18:53:11 -0500

    Eliminacion de  archivos inecesarios en version 0.02

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('[data-toggle="popover"]').popover();
-});
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particlesjs.json');
