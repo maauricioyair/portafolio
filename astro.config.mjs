2e12eed03965becdf45af5a238f8d01f63a8bbfb
Mauricio Yair <yairchavez555@gmail.com>
2023-12-20 13:20:00 -0600

    :wrench: Add Tailwind CSS integration and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+// Astro integrations
+import tailwind from '@astrojs/tailwind';
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(
+      {
+        applyBaseStyles: false,
+      }
+    ),
+  ],
+});
