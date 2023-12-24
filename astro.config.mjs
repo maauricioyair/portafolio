85a2ceacbff0ad2e5efae4775a7d4b883ba4852b
Mauricio Yair <yairchavez555@gmail.com>
2023-12-24 16:08:55 -0600

    :rocket: Add GitHub Pages deployment workflow and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mauricioyair.github.io',
+  base: '/portafolio',
   integrations: [
     tailwind(
       {
