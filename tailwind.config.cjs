823733795caede1a850a8eb6b1a676d6986e4ea0
Mauricio Yair <yairchavez555@gmail.com>
2023-12-24 15:03:14 -0600

    :art: Update global CSS styles and Tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,21 +4,37 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'theme-light': '#fdfdfc',
-        'theme-dark': '#161616',
-        'color-white': '#ffffff',
-        'color-black': '#333333',        
-        'accent': '136, 58, 234',
-        'accent-light': '224, 204, 250',
-        'accent-dark': '49, 10, 101',
-        'gradient': 'linear-gradient(45deg, rgb(var(--accent)), rgb(var(--accent-light)) 30%, white 60%)',
+        'theme-light': 'rgb(255, 255, 255)',
+        'theme-dark': 'rgb(22, 22, 22)',        
+        'color-black-extra': 'rgb(28, 28, 28)',  
+        'color-light-extra': 'rgb(245, 245, 245)',     
+        'accent': 'rgb(255, 69, 0)',
+        'accent-dark': 'rgb(264, 116, 30)',
+        'accent-light': 'rgb(237, 162, 59)',
+        'border-light': 'rgb(222, 222, 222)',
+        'hover-light': 'rgb(225, 255, 255)',
+        'border-dark': 'rgb(40, 40, 40)',
+        'hover-dark': 'rgba(38, 38, 38, 0.88)',              
+        'gradient': 'linear-gradient(45deg, var(--accent), var(--accent-light) 30%, white 60%)',
       },
       boxShadow: {
         'inset': 'inset 0 0 0 1px rgba(255, 255, 255, 0.1)'
       },
       backgroundImage: {
-        'linear-gradient': 'linear-gradient(45deg, rgb(var(--accent)), rgb(var(--accent-light)) 30%, white 60%)'
-      }          
+        'linear-gradient': 'linear-gradient(45deg, var(--accent), var(--accent-light) 30%, white 60%)'
+      },
+      maxWidth: {
+        'cal': 'calc(100% - 2rem)',
+      },
+      animation: {
+        marquee: 'marquee forwards alternate 60s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }        
+      },
     },
   },
   plugins: [],
